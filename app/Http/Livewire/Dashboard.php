2659bb76c9f4fa96810fe01faa42e5247768d3a8
kcacuin <kc.acuin@odecci.com>
2024-08-13 20:41:22 +0800

    Dashboard: Active Members

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Members;
 use Illuminate\Support\Facades\Http;
 
 use Livewire\Component;
@@ -18,10 +19,37 @@ class Dashboard extends Component
     public $toplapses = [];
     public $activecollections = [];
 
+    public function mount()
+    {
+        $data = $this->getData();
+        
+        $mountData = collect($data)->map(function ($d) {
+            return [
+                'activeMemberCount' => $d['members']->count(),
+                // TODO: In-progress
+                'totalLoanBalance' => 0,
+                'totalInterest' => 0,   
+                'totalLoanCollection' => 0,
+                'totalAdvancePayment' => 0,
+                'totalOtherDeductions' => 0,
+                'totalActiveStanding' => 0, 
+                'totalFullPayment' => 0,    
+                'totalCR' => 0,             
+                'totalEndingActiveMember' => 0
+            ];
+        })->first(); 
+
+        $this->data = $mountData;
+
+
+        // dd($this->data);
+    }
+
     public function render()
     {
-        $data = Http::withToken(getenv('APP_API_TOKEN'))->get(getenv('APP_API_URL').'/api/Dashbaord/DashboaredView');   
-        $this->data = isset($data->json()[0]) ? $data->json()[0] : [];
+        // TODO: In-progress
+        // $data = Http::withToken(getenv('APP_API_TOKEN'))->get(getenv('APP_API_URL').'/api/Dashbaord/DashboaredView');   
+        // $this->data = isset($data->json()[0]) ? $data->json()[0] : [];
 
         $getarea =  Http::withToken(getenv('APP_API_TOKEN'))->get(getenv('APP_API_URL').'/api/FieldArea/AreasList');      
         $this->area = $getarea->json();      
@@ -36,5 +64,18 @@ class Dashboard extends Component
         $this->activecollections = $activecollections->json();         
 
         return view('livewire.dashboard');
+    }
+
+    public function getData()
+    {
+        // TODO: In-progress
+
+        $members = Members::query()->where('Status', 1)->get();
+        $data[] = [
+            'members' => $members,
+            'totalSample' => 50,
+        ];
+
+        return $data;
     }
 }
