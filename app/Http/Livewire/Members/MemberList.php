2f090c5c7a4a42a1172228ad1de512ffefa2a71e
ace-caspe <ace.caspe@odecci.com>
2024-08-17 00:50:07 +0800

    updated member details

--- app/Http/Livewire/Members/MemberList.php
+++ app/Http/Livewire/Members/MemberList.php
@@ -56,7 +56,7 @@ class MemberList extends Component
     {
         $members = Members::with(['applications', 'detail', 'loanhistory','fileuploads'])
             ->whereHas('applications', function ($query) {
-                $query->where('Status', 1);
+                $query->whereIn('Status', [7,8,9,10,14,15]);
             })
             ->where(function ($query) {
                 $query->where('Fname', 'like', '%' . $this->keyword . '%')
