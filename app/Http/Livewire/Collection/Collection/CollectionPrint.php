91181db24a429dfb04b01f12cdf1a684bdfcd6bc
ace-caspe <ace.caspe@odecci.com>
2024-11-03 00:43:54 +0800

    update collection print

--- app/Http/Livewire/Collection/Collection/CollectionPrint.php
+++ app/Http/Livewire/Collection/Collection/CollectionPrint.php
@@ -75,7 +75,7 @@ class CollectionPrint extends Component
                 $details['releasingDate']= $application->loanhistory->DateReleased;
                 $details['dueDate']= $application->loanhistory->DueDate;
                 $details['dailyCollectibles']= $application->detail->ApprovedDailyAmountDue;
-                $details['amountDue']= $application->loanhistory->OutstandingBalance + ($application->loanhistory->Penalty) ? $application->loanhistory->Penalty:0;
+                $details['amountDue']= $application->loanhistory->OutstandingBalance + (($application->loanhistory->Penalty) ? $application->loanhistory->Penalty:0);
                 $details['totalSavingsAmount']=  $totalSavings;
                 $details['advancePayment']=  $totalAdvance;
                 $details['lapsePayment']=  $totalLapses;
@@ -87,7 +87,7 @@ class CollectionPrint extends Component
               
                 $this->areaDetails[] = $details;
             }
-            
+            dd($this->areaDetails);
             
             // $this->details['areaName'] = $areaName;
             // $this->details['area_RefNo']= $this->areaRefNo;
